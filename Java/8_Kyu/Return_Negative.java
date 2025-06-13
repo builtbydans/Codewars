609f5d7c3eae5589902d35e25f282c9ea847aa99
builtbydans <danish.shafi@live.co.uk>
2025-06-13 15:01:36 +0100

    june 13th - return negative challenge

---
+++ Java/8_Kyu/Return_Negative.java
@@ -0,0 +1,11 @@
+public class Return_Negative {
+    public static void main(String[] args) {
+        System.out.println(makeNegative(-42));
+        System.out.println(makeNegative(64));
+        System.out.println(makeNegative(0));
+    }
+
+    public static int makeNegative(final int x) {
+        return x > 0 ? -x : x;
+    }
+}
