000698c302093d096c3bcf5fc92e4b4673473b78
builtbydans <danish.shafi@live.co.uk>
2025-07-15 13:48:48 +0100

    jul 15th - 7th kyu SumOfNums 2

--- Java/7_Kyu/SumOfNums.java
+++ Java/7_Kyu/SumOfNums.java
@@ -6,6 +6,8 @@ public class SumOfNums {
         System.out.println(GetSum(-50, 0));
         System.out.println(GetSum(-1, -5));
         System.out.println(GetSum(-5, -5));
+
+        System.out.println(getSum(5, -1));
     }
 
     public static int GetSum(int a, int b) {
@@ -14,5 +16,17 @@ public class SumOfNums {
         int count = end - start + 1;
 
         return (count * (start + end)) / 2;
+    }
+
+    // loop based
+    public static int getSum(int a, int b) {
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
+        int sum = 0;
+
+        for (int i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
