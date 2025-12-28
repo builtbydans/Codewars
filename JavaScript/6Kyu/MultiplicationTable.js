f12c09f39c1864d70866f58b213ceef5514d2bbd
builtbydans <danish.shafi@live.co.uk>
2025-12-28 18:31:24 +0000

    6kyu/multiplicationTable

---
+++ JavaScript/6Kyu/MultiplicationTable.js
@@ -0,0 +1,13 @@
+const multiplicationTable = (size) => {
+  let grid = [];
+
+  for (let i = 1; i <= size; i++) {
+    let row = [];
+    for (let j = 1; j <= size; j++) {
+      row.push(i * j);
+    }
+    grid.push(row);
+  }
+
+  return grid;
+};
