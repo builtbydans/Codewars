1ad371383ed6856d2d2b0c5f41d910baabb38702
Danish Shafi <danish.shafi@live.co.uk>
2026-03-27 17:53:19 +0000

    6kyu - parity outlier

---
+++ Python/5Kyu/directions_reduction.py
@@ -0,0 +1,8 @@
+# Task
+# Write a function dirReduc which will take an array of strings and returns an array of strings with the needless
+#     directions removed (W<->E or S<->N side by side).
+
+def dir_reduce(arr):
+    return arr
+
+print(dir_reduce(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]))
