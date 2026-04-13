af76e4b04a1ce57429bb0de7a06406b1e4629ca8
Danish Shafi <danish.shafi@live.co.uk>
2026-04-13 18:50:43 +0100

    8KYU/SquareSum

---
+++ Python/8Kyu/square_sum.py
@@ -0,0 +1,10 @@
+
+
+def square_sum(numbers):
+    return sum(n ** 2 for n in numbers)
+
+print(square_sum([1,2]))
+print(square_sum([0, 3, 4, 5]))
+print(square_sum([]))
+print(square_sum([-1,-2]))
+print(square_sum([-1,0,1]))
